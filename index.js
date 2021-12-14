4ef82b842d133ebe91d732a55327c685923b2023
abassyan <85851971+abassyan@users.noreply.github.com>
2021-12-14 22:03:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -177,6 +177,7 @@ if(message.content.startsWith(`${prefix}vote`)){
     .setDescription (`
 
  **[Invite](https://discord.com/api/oauth2/authorize?client_id=902939809989922917&permissions=8&scope=bot)**
+
  **[Support](https://discord.gg/gPP8GgQNfB)** 
 
 `)
