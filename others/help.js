7ac54fab8b3164e5c02c9b0a9b18674107ae39f3
abassyan <85851971+abassyan@users.noreply.github.com>
2021-12-14 21:42:49 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -42,8 +42,7 @@ module.exports = {
 
 
  **| Links**
-__**[Support](https://discord.gg/gPP8GgQNfB)**__ **-** __[Vote]
-e)**__ **-** __**[Invite](https://discord.com/api/oauth2/authorize?client_id=910671479576862721&permissions=8&scope=bot)**__ **-** __()__
+__**[Support](https://discord.gg/gPP8GgQNfB)**____ __**[Invite](https://discord.com/api/oauth2/authorize?client_id=910671479576862721&permissions=8&scope=bot)**__ **-** __()__
 `)
    helpEmbed
    message.react("✅")
