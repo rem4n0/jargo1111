574d0328ab9e77724221aff89de7e9418dbeb0f1
abassyan <85851971+abassyan@users.noreply.github.com>
2022-01-07 04:58:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('OTEwNjcxNDc5NTc2ODYyNzIx.YZWO6A.xeHegdhKiEf6lR1JK657Q7MG6ps');
+client.login('OTEwNjcxNDc5NTc2ODYyNzIx.YZWO6A.7OtqiG_3gH5j4IU6okhdUQa0f5k');
 client.commands = new Collection();
 client.setMaxListeners(0);    
 client.prefix = PREFIX;
